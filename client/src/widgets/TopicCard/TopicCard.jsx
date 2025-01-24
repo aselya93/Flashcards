805bf0c74a16920db1cc8c087537b38e5b22bd15
Ekaterina <makhonina.katerina@gmail.com>
2025-01-24 14:44:01 +0300

    topic_style

--- client/src/widgets/TopicCard/TopicCard.jsx
+++ client/src/widgets/TopicCard/TopicCard.jsx
@@ -1,10 +1,19 @@
 export default function TopicCard({ topic }) {
-  const topicsHendler = () => {}
-    
+  const topicsHendler = () => {};
 
   return (
-    <div>
-      <span>{topic.name}</span>,<div>{topic.img}</div>,<span>123</span><button onClick={topicsHendler}>Выбрать</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        border: "1px solid red",
+        width: "500px",
+        height: "500px",
+      }}
+    >
+      <span>{topic.name}</span>
+      <img src={topic.img} alt={topic.name} />,<span>123</span>
+      {/* <button onClick={topicsHendler}>Выбрать</button> */}
     </div>
   );
 }
