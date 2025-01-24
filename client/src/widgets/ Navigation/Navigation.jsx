c0b69794b2e762eebabf4890fd3695f0baa5877f
GMGinIT <gmginit@gmail.com>
2025-01-24 14:46:57 +0300

    navi

--- client/src/widgets/ Navigation/Navigation.jsx
+++ client/src/widgets/ Navigation/Navigation.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-
 import { Link, Outlet, useNavigate } from "react-router-dom";
-
 import { Button } from "antd";
+import styles from "./Navigation.module.css";
 
 function Navigation() {
   const navigate = useNavigate();
-  
 
   return (
-    <div>
-
-        <div >
-          <Button onClick={() => navigate("/")} >
-            <Link to='/topics'>
-            </Link>
+    <>
+      <div className={styles.navigation}>
+        <div className={styles.navLeft}>
+          <Button className={styles.navButton} onClick={() => navigate("/")}>
+            Главная
+          </Button>
+          <Button
+            className={styles.navButton}
+            onClick={() => navigate("/topics")}
+          >
+            Темы викторин
           </Button>
         </div>
-
-       
+      </div>
 
       <Outlet />
-    </div>
+    </>
   );
 }
 
