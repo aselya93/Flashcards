672cdc2ea10d64ee70c554efae64e4e6704bcae9
GMGinIT <gmginit@gmail.com>
2025-01-24 13:56:32 +0300

    exp

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,28 @@
-import { Layout,  } from "antd";
-const {  Footer } = Layout;
-
+import React from "react";
+import { Button } from "antd";
+import { Layout } from "antd";
+const { Footer } = Layout;
+import styles from "./MainPage.module.css";
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
+  const navigate = useNavigate();
+
   return (
-  <div>Hello!</div>
+    <div className={styles.mainPage}>
+      <div className={styles.heroSection}>
+        <h1 className={styles.title}>Игра по флеш-картам</h1>
+        <p className={styles.subtitle}>Проверь свои знания и улучши навыки!</p>
+        <Button
+          className={styles.ctaButton}
+          type="primary"
+          size="large"
+          onClick={() => navigate("/topics")}
+        >
+          Давай поиграем!
+        </Button>
+      </div>
+    </div>
   );
 }
 
